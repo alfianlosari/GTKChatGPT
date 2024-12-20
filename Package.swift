2c510845cf41f1651d891eb826d753def87abb5a
Alfian Losari <alfianlosari@gmail.com>
2024-12-20 14:48:31 +0700

    0.1.0

--- Package.swift
+++ Package.swift
@@ -6,7 +6,9 @@ import PackageDescription
 let package = Package(
     name: "GTKChatGPT",
     dependencies: [
-        .package(url: "https://git.aparoksha.dev/aparoksha/adwaita-swift", branch: "main"),
+        .package(
+            url: "https://git.aparoksha.dev/aparoksha/adwaita-swift",
+            .revision("a0c01362983711b679b9e5c890b2caf5375bee8b")),
         .package(url: "https://github.com/alfianlosari/ChatGPTSwift", from: "2.4.4"),
     ],
     targets: [
